385a4eb5c480b401f7b9822bc73b2ee3c1e6e101
ElHabr, Tony <Anthony.ElHabr@ercot.com>
2017-12-30 09:14:33 -0600

    added zzz.r

---
+++ R/zzz.r
@@ -0,0 +1,45 @@
+
+# References:
+# 1. https://github.com/hrbrmstr/hrbrthemes/blob/master/R/zzz.r.
+# 2. https://github.com/metrumresearchgroup/sinew/blob/master/R/zzz.R
+
+print_pkgstartupmsg <- function(msg) {
+  paste0(strwrap(msg, width = 80), sep = "", collapse = "\n")
+}
+
+# Note that paste0 is not necessary.
+.onAttach <- function(libname, pkgname) {
+  # packageStartupMessage(
+  msg_1 <-
+    paste0(
+    "This package is primarily intended for internal use only,",
+    " though it does not contain any confidential information ",
+    " See https://github.com/aelhabr/ercotr for examples."
+    )
+  packageStartupMessage(
+    # paste0(strwrap(msg1, width = 80), sep = "", collapse = "\n")
+    print_pkgstartupmsg(msg_1)
+  )
+  msg_2 <-
+    paste0(
+    "Note the global variables now loaded into your environment.",
+    " These include `ercot_palette` (which is equivalent to `ercot_palette_w_contrast`)",
+    " and `ercot_palette2` (which is equivalent to to `ercot_palette_wo_contrast`).",
+    " These variables are used in the `scale_color/fill_manual()`",
+    " and `scale_color/fill_manual2()` functions respectively.",
+    " It is not recommended to use the `ercot_pal()` and `ercot_pal2()` functions",
+    " directly. These exist primarily to serve as analogues to the `ggplot2` `manual_pal()` function."
+  )
+  packageStartupMessage(
+    # paste0(strwrap(msg2, width = 80), sep = "", collapse = "\n")
+    print_pkgstartupmsg(msg_2)
+  )
+
+}
+
+# .onAttach <- function(libname, pkgname) {
+#   if (interactive()) {
+#     packageStartupMessage(
+#       paste0("This package is under *active* development."))
+#   }
+# }
